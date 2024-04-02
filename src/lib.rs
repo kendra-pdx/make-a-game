a46e9ba95837c36510d1f12c386943d4f8693f63
Kendra Elford <kendra@enkode.me>
2024-04-02 08:03:43 -0700

    setup for wasm

---
+++ src/lib.rs
@@ -0,0 +1,44 @@
+mod helpers;
+mod basic;
+mod physics;
+
+use basic::BasicPlugin;
+use bevy::prelude::*;
+use physics::Physics;
+use wasm_bindgen::prelude::wasm_bindgen;
+
+fn create_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+fn draw_cursor(
+    camera_query: Query<(&Camera, &GlobalTransform), With<Camera>>,
+    windows: Query<&Window>,
+    mut gizmos: Gizmos,
+) {
+    let Ok((camera, camera_transform)) = camera_query.get_single() else {
+        return;
+    };
+
+    let Some(cursor_position) = windows.single().cursor_position() else {
+        return;
+    };
+
+    // Calculate a world position based on the cursor's position.
+    let Some(point) = camera.viewport_to_world_2d(camera_transform, cursor_position) else {
+        return;
+    };
+
+    gizmos.circle_2d(point, 10., Color::WHITE);
+}
+
+#[wasm_bindgen]
+pub fn start() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(BasicPlugin::new(false))
+        .add_plugins(Physics::new(true, false))
+        .add_systems(Startup, create_camera)
+        .add_systems(Update, draw_cursor)
+        .run();
+}
